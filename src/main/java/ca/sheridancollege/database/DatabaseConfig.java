16b58be590787cf90246eb082c0a1df0ae859140
sujijava <59837559+sujijava@users.noreply.github.com>
2020-11-11 13:23:39 -0500

    cleardb

--- src/main/java/ca/sheridancollege/database/DatabaseConfig.java
+++ src/main/java/ca/sheridancollege/database/DatabaseConfig.java
@@ -12,32 +12,63 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 	@Configuration
 	public class DatabaseConfig {
 
-		//Used in our DatabaseAccess class to submit JDBC Query Strings
-		@Bean
-		public NamedParameterJdbcTemplate 
-				namedParemterJdbcTemplate(DataSource dataSource) {
-			return new NamedParameterJdbcTemplate(dataSource);
-		}
 		
-		//Creates our connection to our database.  In this case h2
+		@Bean
+		public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+		    return new NamedParameterJdbcTemplate(dataSource);
+		}
+
 		@Bean
 		public DataSource dataSource() {
-			DriverManagerDataSource dataSource = new DriverManagerDataSource();
-			dataSource.setDriverClassName("org.h2.Driver");
-			dataSource.setUrl("jdbc:h2:mem:testdb");
-			dataSource.setUsername("sa");
-			dataSource.setPassword("");
-			return dataSource;
+		    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		    dataSource.setUrl("jdbc:mysql://localhost/NBA");
+		    dataSource.setUsername("root");
+		    dataSource.setPassword("root");
+		    
+		    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		    dataSource.setUrl("jdbc:mysql://b95423c21fdcd4:4c8ad120@us-cdbr-east-02.cleardb.com/heroku_0657e10a54b4617?reconnect=true");
+		    dataSource.setUsername("b95423c21fdcd4");
+		    dataSource.setPassword("4c8ad120");
+		    	    
+		    
+		    return dataSource;
 		}
 		
+		
+		
+		
+		
+		
+//		
+//		
+//		
+//		//Used in our DatabaseAccess class to submit JDBC Query Strings
+//		@Bean
+//		public NamedParameterJdbcTemplate 
+//				namedParemterJdbcTemplate(DataSource dataSource) {
+//			return new NamedParameterJdbcTemplate(dataSource);
+//		}
+//		
+//		//Creates our connection to our database.  In this case h2
+//		@Bean
+//		public DataSource dataSource() {
+//			DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//			dataSource.setDriverClassName("org.h2.Driver");
+//			dataSource.setUrl("jdbc:h2:mem:testdb");
+//			dataSource.setUsername("sa");
+//			dataSource.setPassword("");
+//			return dataSource;
+//		}
+//		
 		//Load any default sql files when we compile the project.
-		@Bean
-		public DataSource loadSchema() {
-		    return new EmbeddedDatabaseBuilder()
-		      .setType(EmbeddedDatabaseType.H2)
-		      .addScript("classpath:inventory.sql")
-		      .addScript("classpath:users.sql")
-		      //You can include additional .addScript() for multiple sql files.
-		      .build();
-		}
+//		@Bean
+//		public DataSource loadSchema() {
+//		    return new EmbeddedDatabaseBuilder()
+//		      .setType(EmbeddedDatabaseType.H2)
+//		      .addScript("classpath:inventory.sql")
+//		      .addScript("classpath:users.sql")
+//		      //You can include additional .addScript() for multiple sql files.
+//		      .build();
+//		}
 	}
